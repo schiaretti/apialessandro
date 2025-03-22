f01db94c0814526a5f57c9d749514f3a38fdd561
ronischiaretti <pnamaquina@gmail.com>
2025-03-22 12:58:28 -0300

    arrumei as rotas

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(cors());
 app.use('/', publicRoutes);
 
 // Rotas privadas (protegidas pelo middleware de autenticação)
-app.use('/', auth, privateRoutes);
+app.use('/cadastro_user', auth, privateRoutes);
+app.use('/listar-usuarios', auth, privateRoutes);
 
 
 // Endpoint de saúde
